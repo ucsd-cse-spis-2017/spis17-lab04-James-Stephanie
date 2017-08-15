902b1bfb168e8974da60d41617eec7d304fb49cc
James Cor <jcor@ucsd.edu>
2017-08-15 15:44:08 -0700

    Multiplying without actually multiplying

--- recursiveFuncs.py
+++ recursiveFuncs.py
@@ -1,7 +1,13 @@
 # Implement a function that multiply integers without using the '*' operator
 
 def recProduct(a, b):
-   """ Returns the product of two integers a and b without using the * operator
-       This function should use recursion"""
+   """Returns the product of two integers a and b without using the * operator This function should use recursion"""
+   if (b == 0):
+      return 0
+   if (b == 1):
+      return a
+   if (b < 0):
+      return recProduct(a, b+1) - a
+   return recProduct(a, b-1) + a
+
    
-return 'stub'
--- test_recursiveFuncs.py
+++ test_recursiveFuncs.py
@@ -3,15 +3,14 @@
 import unittest
 from recursiveFuncs import recProduct 
 
-
 class Test_recProduct(unittest.TestCase):
     
     def test_recProduct_1(self):
         self.assertAlmostEqual(recProduct(0,5),0)
-
-    # Note to students: add more of these test cases
- 
-        
+    def test_recProduct_2(self):
+        self.assertAlmostEqual(recProduct(1,5),5)
+    def test_recProduct_3(self):
+        self.assertAlmostEqual(recProduct(1,-5),-5)
         
 if __name__ == '__main__':
-unittest.main()
+    unittest.main()
